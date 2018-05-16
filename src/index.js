e3b16e6f242cddbe27a589c6377c5714acbbb667
Tyler Long <tyler4long@gmail.com>
2018-05-16 16:23:27 +0800

    Navigate to api endpoint

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ const DEBUG = true
 ;(async () => {
   const browser = await puppeteer.launch({ headless: !DEBUG })
   const page = await browser.newPage()
+  page.setDefaultNavigationTimeout(20000)
+  page.setViewport({ width: 1280, height: 800 })
+
   await page.goto('https://dash.readme.io/login', { waitUntil: 'networkidle2' })
 
   const emailSelector = '#email'
@@ -20,9 +23,17 @@ const DEBUG = true
   await page.click(loginButtonSelector)
   await page.waitForNavigation({ waitUntil: 'networkidle2' })
 
-  const enterpriseSelector = '#all-projects > div.container-main.container-main-projects > div:nth-child(6) > div:nth-child(1) > a'
-  await page.click(enterpriseSelector)
-  await page.waitForNavigation({ waitUntil: 'networkidle2' })
+  // const enterpriseSelector = '#all-projects > div.container-main.container-main-projects > div:nth-child(6) > div:nth-child(1) > a'
+  // await page.click(enterpriseSelector)
+  // await page.waitForNavigation({ waitUntil: 'networkidle2' })
+
+  const endpointUri = `https://dash.readme.io/project/${process.env.README_IO_API_SLUG}/refs/getversioninfo`
+  try {
+    await page.goto(endpointUri, { waitUntil: 'networkidle0' })
+  } catch (e) {
+    console.log(e.message)
+    // ignore Navigation Timeout Exceeded
+  }
 
   await page.screenshot({path: 'temp.png'})
   await browser.close()
